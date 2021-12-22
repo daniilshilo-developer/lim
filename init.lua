6d17b24e3d1894a2f25fdd7c9d5cb455b8856ee7
Daniil Shilo <daniilshilo.developer@gmail.com>
2021-12-23 01:00:40 +0300

    feat(fold): Добавил вложенность в init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Импортирование других файлов {{{
 -- Плагины
 require('plugins')
 
@@ -7,6 +8,9 @@ require('keybindings')
 -- Мои твики
 require('tweaks')
 
+-- }}}
+
+-- Developer Interface : Тема {{{
 -- Конфигурация цветовой схемы
 vim.opt.syntax = 'enable'
 vim.opt.background = 'dark'
@@ -15,63 +19,83 @@ vim.cmd('colorscheme gruvbox-material')
 -- Кодировка
 vim.opt.encoding = 'utf-8'
 
--- Конфигурация табов
+-- Подчёркиваем линию, на которой находится курсор
+vim.opt.cursorline = true
+
+-- Высота статуса
+vim.opt.cmdheight = 1
+
+---}}}
+
+-- Developer Interface : Пробельные знаки {{{
+-- Не показываем пробельные знаки
+vim.opt.list = false
+vim.opt.listchars = [[tab:¦\ ,space:·,precedes:«,extends:»]]
+-- }}}
+
+-- Developer Experience : Конфигурация табов {{{
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = false
 vim.opt.copyindent = true
+-- }}}
 
--- Подчёркиваем линию, на которой находится курсор
-vim.opt.cursorline = true
-
--- Делаем так, чтобы поведение Backspace было ожидаемым
-vim.opt.backspace = 'indent,eol,start'
-
--- Переключение между буфферами без сохранения
-vim.o.hidden = true
-
--- Вложенность
-vim.opt.foldmethod = 'syntax'
-vim.opt.foldlevel = 99
-
--- Высота статуса
-vim.opt.cmdheight = 1
-
--- Фикс для скролла
-vim.opt.scrolloff = 5
-
--- Поиск
+-- Developer Experience : Поиск {{{
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.wrapscan = true
 vim.opt.cursorline = true
+-- }}}
 
+-- Developer Experience : Буферы {{{
+-- Переключение между буфферами без сохранения
+vim.o.hidden = true
+-- }}}
+
+-- Developer Experience : Вложенность {{{
+-- Вложенность
+vim.opt.foldmethod = 'marker'
+vim.opt.foldlevel = 99
+-- }}}
+
+-- Developer Experience : Быстродействие {{{
 -- Перерисовка терминала
 vim.opt.ttyfast = true
 vim.opt.lazyredraw = true
+-- }}}
 
+-- Developer Experience : Скролл {{{
+-- Фикс для скролла
+vim.opt.scrolloff = 5
+-- }}}
+
+-- Developer Experience : Расположение новых экранов {{{
 -- Правильное разделение окон
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+-- }}}
 
--- Не показываем пробельные знаки
-vim.opt.list = false
-vim.opt.listchars = [[tab:¦\ ,space:·,precedes:«,extends:»]]
-
+-- Developer Experience : Бэкапы {{{
 -- Избавляемся от бэкапов
 vim.opt.backup = false
 vim.opt.swapfile = false
+-- }}}
 
+-- Developer Experience : Буфер обмена {{{
 -- Используется системный буфер обмена
 vim.opt.clipboard = 'unnamedplus'
+-- }}}
 
+-- Developer Experience : Прочее {{{
 -- Не добавляем новую линию в конце
 vim.opt.fixendofline = false
 
 -- Говорим Neovim, чтобы он нам помогал завершать фразы (автодополнение)
 vim.opt.completeopt = { 'menuone', 'noinsert' }
+-- }}}
 
-
+-- Данная строка нужна для того, чтобы (Neo)Vim разделял код по вложенности и маркерам '{{{' и '}}}'
+-- vim:ts=2:ai:foldmethod=marker:foldlevel=0:
